b9951083b3101beb56330bc4293106058db999c2
EmmanuelArethens <emmanuel.arethens1@gmail.com>
2017-10-24 11:51:08 +0200

    maj

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <html lang="en">
 
 <head>
@@ -5,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="indexstyle.css">
-    <title>Manu Arethens "Lanikeu"</title>
+    <title>Emmanuel Arethens</title>
 </head>
 
 <body>
@@ -13,13 +14,13 @@
 <img src="images/logoEA.png" alt="logoEA"> 
 <h1> Emmanuel Arethens </h1>
 <img src="images/logo2.png" alt="logo">
-
 </section>
 
 <section id="aboutme">
-<h2> About me </h2>
-<p> Codageur de qualité allemande, j'adore codager toute la journée.</p>
-<p> Joueur professionnel d'Overwatch a mes heures perdues je te PL diamant contre 150€</p>
+<h2> A propos</h2>
+<h3> Jeune développeur web en recherche d'entreprise pour un contrat de professionalisation</h3>
+<h2> Technos </h2>
+<h3> HTML / CSS / Javascript / PHP / Angular / MySQL </h3>
 </section>
 
 
@@ -32,7 +33,7 @@ echo '<ul id ="listeprojets">';
         if($dossier!=(".") && $dossier!=("..") && $dossier!=("images"))
         if (is_dir($dossier)) { 
         echo '<li><a href="'.$dossier.'">'.$dossier. '</a></li>';
-        echo '<img class="screenshot" src="'.$dossier.'/screenshot.png" alt="screen">' ;
+        echo '<img src="'.$dossier.'/screenshot.png" class="screenshot" alt="screen">' ;
     }}
 echo '</ul>';
 ?>
@@ -41,7 +42,7 @@ echo '</ul>';
 
 <section id="contact">
 <h2> Contact </h2>
-
+<h3><a href="https://drive.google.com/open?id=0Bxo4nnRB07g5cE1EeV9iUkUzZ2s" id="cv">Lien vers CV</a></h3>
 <h3> Telephone : 06.37.83.62.69 </h3> 
 <h3> Mail : emmanuel.arethens1@gmail.com </h3> 
 <a href="https://github.com/EmmanuelArethens"> <img src="images/github.png" alt="github" id="git"> </a>
